920f1b05a08bee8a250fb3f927c50a80120bfdf4
NikolayNPanev <nikolay.n.panev@gmail.com>
2022-10-27 15:50:43 +0300

    VS Project and DB(Database1.mdb)
    things to do IN ORDER:
    -confirm db connection works on any machine in 308TB
    -Create UI for user registration, login and rent.
    -Hook up UI to DB
    -Make a UI for User Profile
    -Hook up User Profile so it can read the User Data from the DB

---
+++ TSPLibrary/TSPLibrary/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSPLibrary
+{
+    class Author
+    {
+        public int id { get; set; }
+        public string author { get; set; }
+
+        
+
+    }
+}
---
+++ TSPLibrary/TSPLibrary/Connection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.OleDb;
+
+namespace TSPLibrary
+{
+    class Connection
+    {
+        OleDbConnection connect;
+        OleDbCommand command;
+
+        private void ConnectionTo() {
+            connect = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database1.mdb");
+            command = connect.CreateCommand();
+
+        }
+
+        public Connection() {
+            ConnectionTo();
+        }
+
+        public void Insert(Author author)
+        {
+            try
+            {
+                command.CommandText = "INSERT INTO Author(_Author) values ('" + author.author+ "');";
+                command.CommandType = System.Data.CommandType.Text;
+                connect.Open();
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.ToString());
+            }
+            finally {
+                if (connect != null) {
+                    connect.Close();
+                }            
+            }
+        }
+    }
+}
